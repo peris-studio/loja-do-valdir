6b43d3f33fa6a66982add3e89553ac2e5ef67492
Hisllaylla <hisllayllak@gmail.com>
2024-04-23 22:47:47 -0300

    [feat]: adição do swagger à documentação.

--- src/LojaDoValdir/Program.cs
+++ src/LojaDoValdir/Program.cs
@@ -1,7 +1,24 @@
-﻿var builder = WebApplication.CreateBuilder(args);  
+﻿var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();  
+// adicionando swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "LojaDoValdir", Version = "v1" });
+});
 
-app.MapGet("/", () => "Hello, World!");  
+// pipeline
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "LojaDoValdir v1");
+    c.RoutePrefix = string.Empty; // acessar documentação raiz 
+});
 
-app.Run();  
+// rotas
+app.MapGet("/api/rota1", () => "Hello, World!").WithTags(new[] { " Grupo 1 "});
+app.MapGet("/api/rota2", () => "¡Hola, Mundo!").WithTags(new[] { " Grupo 1 "});;
+app.MapPost("/api/rota3", (string mensagem) => $"Mensagem recebida com sucesso: {mensagem}!").WithTags(new[] { " Grupo 2 "});;
+
+app.Run();
